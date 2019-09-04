9a7f2e57a39c49f6560fbb81bc9b70d8e15d21d2
Max Base <MaxBaseCode@Gmail.Com>
2019-09-04 17:51:05 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // https://github.com/BaseMax/LiveChatSocket
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 5000;
+
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('A user connected');
+	socket.on('disconnect', function(){
+		console.log('A user disconnected');
+	});
+	socket.on('chat message', function(author, message){
+		io.emit('chat message', author, message);
+	});
+});
+
+http.listen(port, function(){
+	console.log('Listening on *:' + port);
+});
